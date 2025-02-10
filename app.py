bacb5a11b309294988fde9704aeaa37e52b23aab
Fabio Zheng <zheng.fabio@iisgalvanimi.edu.it>
2025-02-10 11:50:21 +0000

    Spotify 2

--- app.py
+++ app.py
@@ -14,7 +14,8 @@ sp_oauth = SpotifyOAuth(
     client_id=SPOTIFY_CLIENT_ID,
     client_secret=SPOTIFY_CLIENT_SECRET,
     redirect_uri=SPOTIFY_REDIRECT_URI,
-    scope="user-read-private" #permessi x informazioni dell'utente
+    scope="user-read-private", #permessi x informazioni dell'utente
+    show_dialog=True #forziamo la richiesta di inserire new credenziali
 )
 @app.route('/')
 def login():
@@ -31,10 +32,17 @@ def home():
     token_info = session.get('token_info', None) #recupero token sissione (salvato prima)
     if not token_info:
         return redirect(url_for('login'))
+
+    
     sp = spotipy.Spotify(auth=token_info['access_token']) #usiamo il token per ottenere i dati del profilo
     user_info = sp.current_user()
+    playlists = sp.current_user_playlists() #sempre tramite il token sp preso prima
+    playlists_info = playlists['items'] #prendiamo solo la lista delle playlist
     print(user_info) #capiamo la struttura di user_info per usarle nel frontend
-    return render_template('home.html', user_info=user_info) #passo le info utente all'home.html
-#avvio dell'app Flask
+    return render_template('home.html', user_info=user_info, playlists=playlists_info)#avvio dell'app Flask
+@app.route('/logout')
+def logout():
+    session.clear() #cancelliamo l'access token salvato in session
+    return redirect(url_for('login'))
 if __name__ == '__main__':
     app.run(debug=True)
